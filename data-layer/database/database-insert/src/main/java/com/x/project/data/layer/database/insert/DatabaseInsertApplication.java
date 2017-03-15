60bf116070e38bb27623e6bd98d00f4b16d44045
Esteban <ecristobalr@gmail.com>
2017-03-15 16:07:53 +0100

    Added database insert microservice

---
+++ data-layer/database/database-insert/src/main/java/com/x/project/data/layer/database/insert/DatabaseInsertApplication.java
@@ -0,0 +1,28 @@
+package com.x.project.data.layer.database.insert;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.ImportResource;
+
+/**
+ * Main class for database insert microservice JAR.
+ * 
+ * @author Esteban Cristóbal
+ */
+@SpringBootApplication
+@EnableEurekaClient
+@ImportResource("classpath:/META-INF/spring/database-insert*.xml")
+public class DatabaseInsertApplication {
+
+    /**
+     * Main method.
+     * 
+     * @param args
+     *            program arguments
+     */
+    public static void main(String[] args) {
+        SpringApplication.run(DatabaseInsertApplication.class, args);
+    }
+
+}
